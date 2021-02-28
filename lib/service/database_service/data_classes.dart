41f54c038a71443b0476af588a9e5acc8c815a54
GAUTAMSHETA <78837362+GAUTAMSHETA@users.noreply.github.com>
2021-02-28 20:40:39 +0530

    Add files via upload

--- lib/service/database_service/data_classes.dart
+++ lib/service/database_service/data_classes.dart
@@ -1,1 +1,89 @@
+class PendingData {
+  String name, key;
+  int transection;
+  double amount;
 
+  PendingData({this.name, this.transection, this.amount, this.key});
+}
+
+class PersonAllData {
+  int totalDay;
+  double rate,
+      ammount,
+      cuttentInterest,
+      lastPaymentInterast,
+      actualAmount,
+      payedAmount,
+      payedIntrest,
+      remainingAmount,
+      totalAmount;
+  DateTime fromDate, toDate, lastPaymentDate;
+  String key;
+
+  PersonAllData(
+      {this.ammount,
+      this.rate,
+      this.fromDate,
+      this.toDate,
+      this.actualAmount,
+      this.lastPaymentDate,
+      this.lastPaymentInterast,
+      this.payedAmount,
+      this.payedIntrest,
+      this.key}) {
+    totalDay = DateTime.now().difference(fromDate).inDays;
+
+    int day = DateTime.now().difference(lastPaymentDate).inDays;
+
+    cuttentInterest = lastPaymentInterast +
+        (((actualAmount * rate * day) / (100 * 356) * 100).toInt()) / 100;
+
+    totalAmount = ammount + cuttentInterest;
+
+    remainingAmount = totalAmount - payedAmount;
+  }
+}
+
+class PersonCompleteData {
+  int totalDay;
+  double rate,
+      ammount,
+      cuttentInterest,
+      lastPaymentInterast,
+      actualAmount,
+      payedAmount,
+      payedIntrest,
+      remainingAmount,
+      totalAmount;
+  DateTime fromDate, toDate, lastPaymentDate;
+  String key;
+
+  PersonCompleteData(
+      {this.ammount,
+        this.rate,
+        this.fromDate,
+        this.toDate,
+        this.actualAmount,
+        this.lastPaymentDate,
+        this.lastPaymentInterast,
+        this.payedAmount,
+        this.payedIntrest,
+        this.key}) {
+    totalDay = toDate.difference(fromDate).inDays;
+
+    int day = toDate.difference(lastPaymentDate).inDays;
+
+    cuttentInterest = lastPaymentInterast +
+        (((actualAmount * rate * day) / (100 * 356) * 100).toInt()) / 100;
+
+    totalAmount = ammount + cuttentInterest;
+
+    remainingAmount = totalAmount - payedAmount;
+  }
+}
+
+class History{
+  DateTime date;
+  double amount;
+  History({this.amount,this.date});
+}
