1fd733e8d4ae1a2bafb4243189dcaeb586010079
GAUTAMSHETA <78837362+GAUTAMSHETA@users.noreply.github.com>
2021-02-28 20:36:47 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:interest_cal/auth/signUp.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Interest Calcy",
+      theme: ThemeData.dark(),
+      color: Colors.white,
+      // home: LogInPage(),
+      home: SignUpPage(),
+      // home: AddTransactionForDifferentPerson(),
+    );
+  }
+}
